aa67e49a7398db2829875c07070a1ad179f166f1
couper.york <couper.york@hotmail.com>
2017-10-18 16:45:10 +1300

    Initial Commit.

---
+++ ListViewiOSAnimationBug/ListViewiOSAnimationBug/Converters/IsSelectedOrganisationConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace ListViewiOSAnimationBug.Converters
+{
+    using System;
+    using System.Globalization;
+    using Xamarin.Forms;
+
+    public class IsSelectedOrganisationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "OrganisationsTickIcon" : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ListViewiOSAnimationBug/ListViewiOSAnimationBug/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ListViewiOSAnimationBug
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
